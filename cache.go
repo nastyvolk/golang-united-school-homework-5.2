d8349e7a987644d8fe27116a6eb8cd50bb054785
nastyavolk <nastya.vooolk@gmail.com>
2022-05-21 18:31:21 +0300

    homework-5.2

--- cache.go
+++ cache.go
@@ -3,21 +3,43 @@ package cache
 import "time"
 
 type Cache struct {
+	kvPairs map[string]cacheValue
+}
+
+type cacheValue struct {
+	value    string
+	deadline time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{kvPairs: make(map[string]cacheValue)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	value, ok := c.kvPairs[key]
+	if !ok {
+		return "", false
+	}
+	if value.deadline.IsZero() || value.deadline.After(time.Now()) {
+		return value.value, true
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	val := cacheValue{value: value}
+	c.kvPairs[key] = val
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var keys []string
+	for k := range c.kvPairs {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	val := cacheValue{value: value, deadline: deadline}
+	c.kvPairs[key] = val
 }
